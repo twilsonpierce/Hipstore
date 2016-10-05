291916439d093088b1176ca8586c59ecfe250590
Carlos Martinez <martinezcj2@gmail.com>
2016-10-05 17:55:45 -0400

    Latest updates

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,7 +5,7 @@ import BottomFeature from './components/homepage/bottomFeature'
 const HomePage = React.createClass({
   render() {
     return (
-      <div>
+      <div className="overFlowFix">
       <div className="homeShape">XXXXXX</div>
       <HomeBody />
       <BottomFeature scrollLeft={this.props.scrollLeft} scrollRight={this.props.scrollRight} data={this.props.data} bottomI={this.props.bottomFeatureI}/>
